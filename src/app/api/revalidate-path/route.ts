9c9a991e03dc6acd1e87ed729a856a18204b5a72
Daniel Honzik <dhonzik@gmail.com>
2024-01-02 22:53:14 +0100

    Change query name for revalidate-path

--- src/app/api/revalidate-path/route.ts
+++ src/app/api/revalidate-path/route.ts
@@ -2,10 +2,10 @@ import { revalidatePath } from 'next/cache';
 import type { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
-  const slug = req.nextUrl.searchParams.get('slug');
-  const path = slug ? `/${slug}` : '/[slug]';
+  const value = req.nextUrl.searchParams.get('path');
+  const path = value ? `/${value}` : '/[slug]';
 
   revalidatePath(path, 'page');
 
-  return Response.json({ revalidated: path, type: 'path (well, next.js kind of tag)' });
+  return Response.json({ revalidated: path, type: 'path' });
 }
